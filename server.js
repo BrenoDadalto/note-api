90f6f2954d7a6ca2c076ba87f15fa48b185d046d
Breno Dadalto <brenodadalto@hotmail.com>
2023-05-06 16:23:19 -0300

    server/vercel config update

--- server.js
+++ server.js
@@ -1,29 +1,22 @@
-require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config();
 const notesRoutes = require('./routes/notesRoutes');
+
 const app = express();
 
-const Note = require("./models/Note");
-
 // middleware leitor de json
-app.use(
-    express.urlencoded({
-        extended: true,
-    }),
-)
-
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 
 app.use('/note', notesRoutes);
 
 // rota inicial - endpoint
 app.get('/', (req, res) => {
-    res.json({ message: "Oi, você conseguiu chegar até aqui!" })
+    res.send({ message: "Oi, você conseguiu chegar até aqui!" })
 })
 
-// entrega uma porta
-const MONGODB_URL = process.env.MONGODB_URL
+const MONGODB_URL = process.env.MONGODB_URL;
 
 mongoose.connect(MONGODB_URL)
     .then(() => {
@@ -31,5 +24,5 @@ mongoose.connect(MONGODB_URL)
         app.listen(3000);
     })
     .catch((err) => {
-        console.log("Erro ao conectar no banco ", err);
+        console.log("Erro ao conectar no banco: ", err);
     })
